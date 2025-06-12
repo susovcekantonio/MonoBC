95baba819d3a67ab33550c96c80c9badad30d099
Antonio <asusovcek0@gmail.com>
2025-06-12 11:59:53 +0200

    Added Autofac DI

--- BootcampApp/WebAPI/Program.cs
+++ BootcampApp/WebAPI/Program.cs
@@ -2,27 +2,59 @@ using Repository.Interface;
 using Repository;
 using Service.Interface;
 using Service.ServiceImpl;
+using Autofac;
+using WebAPI.Controllers;
+using Autofac.Extensions.DependencyInjection;
 
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
-builder.Services.AddScoped<IPatientRepository, PatientRepository>();
-builder.Services.AddScoped<IMedicalRecordRepository, MedicalRecordRepository>();
+//builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
+//builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+//builder.Services.AddScoped<IMedicalRecordRepository, MedicalRecordRepository>();
 
-builder.Services.AddScoped<IDoctorService, DoctorService>();
-builder.Services.AddScoped<IPatientService, PatientService>();
-builder.Services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+//builder.Services.AddScoped<IDoctorService, DoctorService>();
+//builder.Services.AddScoped<IPatientService, PatientService>();
+//builder.Services.AddScoped<IMedicalRecordService, MedicalRecordService>();
 
-var app = builder.Build();
+//var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+
+var containerBuilder = new ContainerBuilder();
+
+    containerBuilder.RegisterType<DoctorRepository>().As<IDoctorRepository>();
+    containerBuilder.RegisterType<PatientRepository>().As<IPatientRepository>();
+    containerBuilder.RegisterType<MedicalRecordRepository>().As<IMedicalRecordRepository>();
+
+    containerBuilder.RegisterType<DoctorService>().As<IDoctorService>();
+    containerBuilder.RegisterType<PatientService>().As<IPatientService>();
+    containerBuilder.RegisterType<MedicalRecordService>().As<IMedicalRecordService>();
+
+var app = containerBuilder.Build();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -36,3 +68,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+//var builder = new ContainerBuilder();
+//builder.RegisterType<DoctorService>().As<IDoctorService>();
+//builder.RegisterType<DoctorRepository>().As<IDoctorRepository>();
+//builder.RegisterType<DoctorController>();
+
+//var app = builder.Build();
